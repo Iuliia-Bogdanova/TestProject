38b4929bace5f9289e2be101c296a6be364787b2
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-03-12 23:49:53 +0400

    cards with animation and slider doing

--- words/src/App.js
+++ words/src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+import { useState, useE } from 'react'
+import words from './components/data/words.json'
+import Cards from './components/Cards';
+import './App.css'
 
-function App() {
+
+export default function App() {
+  const [count,setCount] = useState(0)
+
+  function handlePrev(){
+    setCount(count - 1)
+  }
+
+  function handleNext() {
+    setCount(count + 1);
+  }
+  console.log(words [count]);
+
   return (
-    <div className="App">
-      
+    <div className="container">
+      <button className="btn" onClick={handlePrev}>
+        prev
+      </button>
+      <Cards words={words[count]} count={count} key={count} />
+      <button className="btn" onClick={handleNext}>
+        next
+      </button>
     </div>
   );
 }
 
-export default App;
+
