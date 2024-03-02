e9131a87dddff2f2a256da10f140d8f553530255
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2024-03-02 15:51:49 +0400

    menu doing (opacity)

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,16 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+const modalOpen = document.querySelector('#modalOpen');
+const modalClose = document.querySelector('#modalClose');
+const mobileNav = document.querySelector('.mobile-nav');
+
+function openMenu() {
+    mobileNav.style.display = 'block'; 
+    modalOpen.style.display = 'none'; 
+}
+
+function closeMenu() {
+    mobileNav.style.display = 'none'; 
+    modalOpen.style.display = 'block'; 
+}
+
+modalOpen.addEventListener('click', openMenu);
+modalClose.addEventListener('click', closeMenu);
